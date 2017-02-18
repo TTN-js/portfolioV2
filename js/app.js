7ad90db863ebe2c1939d546389a9299c06c4cc62
ttn_js <ttn.js001@gmail.com>
2017-02-18 02:41:54 -0600

    type out and button scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
-$( document ).ready(function() {
-	alert('ready!');
+// // jQuery check
+// window.onload = function() {
+//     if (window.jQuery) {  
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
+
+// type it out
+var inputBox = document.querySelector("#mytypingText"),
+	myString = "web developer & design enthusiast",
+	myArray = myString.split(""),
+	loopTimer;
+
+var frameLoop = function() {
+    if (myArray.length > 0) {
+        inputBox.innerHTML += myArray.shift()
+    } else {
+        clearTimeout(loopTimer)
+    }
+    var loopTimer = setTimeout("frameLoop()", 90)
+}
+
+$( document ).ready(function(){
+	frameLoop();
 });
+
+// scroll to navbar
+$( 'button' ).click(function(){
+	window.scroll({ top: window.innerHeight, left: 0, behavior: 'smooth' });
+});
+
+// scroll back to the top
+// window.scroll({ top: 0, left: 0, behavior: 'smooth' });
